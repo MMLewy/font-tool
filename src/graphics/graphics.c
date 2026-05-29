53543a4f3a4f472e72ac7a1cb3fb19db5cb38957
Kamil Lewandowski <kamil.lewandowski1997@gmail.com>
2026-05-29 15:08:05 +0200

    Implement basic graphics functionality. Vulkan instance and device creation.

--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -5,8 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Graphics_queue_properties_t
+{
+    uint32_t index;
+    VkQueueFamilyProperties properties; 
+
+} Graphics_queue_properties;
+
 #ifdef DEBUG
-#include <stdio.h>
 
 static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(
     VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
@@ -15,46 +21,149 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(
     void* pUserData)
 {
 
-    printf("validation layer: %s\n", pCallbackData->pMessage);
+    g_printf("validation layer: %s\n", pCallbackData->pMessage);
 
     return VK_FALSE;
 }
+
+static VkDebugUtilsMessengerCreateInfoEXT debug_messenger_create_info =
+{
+    .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+    .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+    .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+    .pfnUserCallback = debug_callback,
+    .pUserData = NULL,
+};
 
 VkDebugUtilsMessengerEXT debug_messenger;
 
 #endif
 
+// TODO: MID_PRIO Future plans - redo graphics module in such manner so it works using state machine.
+// State machine structure would help properly reinitialize whole graphics module.
 
-static VkInstance vulkan_instance;
+// Private global variables.
+static VkInstance graphics_vulkan_instance = VK_NULL_HANDLE;
+static VkDevice graphics_logical_device = VK_NULL_HANDLE;
 
 
+// Private functions declarations.
+static Graphics_error graphics_create_vulkan_instance();
+static VkPhysicalDevice graphics_pick_device(
+                                            const char* desired_extensions[],
+                                            uint32_t desired_extensions_count,
+                                            VkPhysicalDeviceFeatures* desired_features);
+static Graphics_queue_properties graphics_pick_queue_family(VkPhysicalDevice device);
 
 Graphics_error graphics_init()
 {
+    VkResult vk_result = VK_SUCCESS;
+
     if(volkInitialize() == VK_ERROR_INITIALIZATION_FAILED) return GRAPHICS_VULKAN_INITIALIZATION;
 
+    Graphics_error result = graphics_create_vulkan_instance();
+    if(result != GRAPHICS_OK) return result;
+
+    volkLoadInstanceOnly(graphics_vulkan_instance);
+
+#ifdef DEBUG
+    vk_result = vkCreateDebugUtilsMessengerEXT(graphics_vulkan_instance, &debug_messenger_create_info, NULL, &debug_messenger);
+    assert(vk_result == VK_SUCCESS);
+#endif
+
+    const char* desired_extensions[] = 
+    {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+        VK_KHR_RAY_TRACING_PIPELINE_EXTENSION_NAME,
+        VK_KHR_SPIRV_1_4_EXTENSION_NAME,
+        VK_KHR_ACCELERATION_STRUCTURE_EXTENSION_NAME,
+        VK_KHR_SHADER_FLOAT_CONTROLS_EXTENSION_NAME,
+        VK_EXT_DESCRIPTOR_INDEXING_EXTENSION_NAME,
+        VK_KHR_BUFFER_DEVICE_ADDRESS_EXTENSION_NAME,
+        VK_KHR_DEFERRED_HOST_OPERATIONS_EXTENSION_NAME
+    };
+    uint32_t desired_extensions_count = sizeof(desired_extensions) / sizeof(char*);
+
+    VkPhysicalDeviceFeatures desired_features = {};
+    desired_features.geometryShader = VK_TRUE;
+    desired_features.tessellationShader = VK_TRUE;
+
+    VkPhysicalDevice physical_device = graphics_pick_device(desired_extensions, desired_extensions_count, &desired_features);
+    if(physical_device == VK_NULL_HANDLE) return GRAPHICS_VULKAN_NO_USABLE_PHYSICAL_DEVICE;
+
+#ifdef DEBUG
+    VkPhysicalDeviceProperties properties;
+    vkGetPhysicalDeviceProperties(physical_device, &properties);
+    
+    g_printf("Chosen device's name: %s\n", properties.deviceName);
+#endif
+
+    Graphics_queue_properties queue_family_properties = graphics_pick_queue_family(physical_device);
+    if(queue_family_properties.index == UINT32_MAX) return GRAPHICS_VULKAN_NO_USABLE_QUEUE_FAMILY;
+
+    float queue_priorities[2] = {1.0f, 0.5f};
+
+    VkDeviceQueueCreateInfo queue_create_info = 
+    {
+        .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        .pNext = VK_NULL_HANDLE,
+        .flags = 0,
+        .queueFamilyIndex = queue_family_properties.index,
+        .queueCount = 2,
+        .pQueuePriorities = queue_priorities
+    };
+
+    VkDeviceCreateInfo device_create_info =
+    {
+        .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+        .pNext = VK_NULL_HANDLE,
+        .flags = 0,
+        .queueCreateInfoCount = 1,
+        .pQueueCreateInfos = &queue_create_info,
+        .enabledLayerCount = 0,
+        .ppEnabledLayerNames = NULL,
+        .enabledExtensionCount = desired_extensions_count,
+        .ppEnabledExtensionNames = desired_extensions,
+        .pEnabledFeatures = &desired_features
+    };
+
+    vk_result = vkCreateDevice(physical_device, &device_create_info, NULL, &graphics_logical_device);
+    if(vk_result != VK_SUCCESS) return GRAPHICS_VULKAN_LOGICAL_DEVICE_CREATE;
+
+    volkLoadDevice(graphics_logical_device);
+
+    return GRAPHICS_OK;
+}
+
+void graphics_cleanup()
+{
+    if(graphics_logical_device != VK_NULL_HANDLE) vkDestroyDevice(graphics_logical_device, NULL);
+
+#ifdef DEBUG
+    if(graphics_vulkan_instance != VK_NULL_HANDLE) vkDestroyDebugUtilsMessengerEXT(graphics_vulkan_instance, debug_messenger, NULL);
+#endif
+
+    if(graphics_vulkan_instance != VK_NULL_HANDLE) vkDestroyInstance(graphics_vulkan_instance, NULL);
+
+    volkFinalize();
+}
+
+
+// Private functions
+static Graphics_error graphics_create_vulkan_instance()
+{
 #ifdef DEBUG
     const char* desired_layers[] = 
     {
         "VK_LAYER_KHRONOS_validation",
     };
     uint32_t desired_layers_count = sizeof(desired_layers) / sizeof(char*);
-
-    VkDebugUtilsMessengerCreateInfoEXT debug_messenger_create_info =
-    {
-        .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
-        .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
-        .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
-        .pfnUserCallback = debug_callback,
-        .pUserData = NULL,
-    };
-
 #endif
 
-    // TODO: Enhance in the future the extension selection on a different platforms. 
+    // TODO: MID_PRIO Enhance in the future the extension selection on a different platforms. 
     const char* desired_extensions[] = 
     {
-        "VK_KHR_surface",
+        VK_KHR_SURFACE_EXTENSION_NAME,
 #ifdef _WIN32
         "VK_KHR_win32_surface",
 #elifdef __APPLE__
@@ -70,6 +179,7 @@ Graphics_error graphics_init()
         VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
 #endif
     };
+
     uint32_t desired_extensions_count = sizeof(desired_extensions) / sizeof(char*);
 
     uint32_t extension_count = 0;
@@ -78,7 +188,7 @@ Graphics_error graphics_init()
     VkExtensionProperties* extensions = (VkExtensionProperties*)malloc(extension_count * sizeof(VkExtensionProperties));
     vkEnumerateInstanceExtensionProperties(NULL, &extension_count, extensions);
 
-    // TODO: Improve this loop
+    // TODO: LOW_PRIO Improve this loop
     uint32_t found_extension_count = 0;
     for(int i = 0; i < desired_extensions_count; i++)
     {
@@ -92,7 +202,7 @@ Graphics_error graphics_init()
     }
     free(extensions);
 
-    if(found_extension_count != desired_extensions_count) return GRAPHICS_VULKAN_PRESENTATION_EXTENSIONS;
+    if(found_extension_count != desired_extensions_count) return GRAPHICS_VULKAN_NO_INSTANCE_EXTENSIONS;
 
     VkApplicationInfo app_info = 
     {
@@ -102,7 +212,7 @@ Graphics_error graphics_init()
         .applicationVersion = APPLICATION_VERSION,
         .pEngineName = APPLICATION_NAME,
         .engineVersion = APPLICATION_VERSION,
-        .apiVersion = VK_API_VERSION_1_0
+        .apiVersion = VK_API_VERSION_1_1
     };
 
     VkInstanceCreateInfo instance_info =
@@ -128,25 +238,141 @@ Graphics_error graphics_init()
 
     };
 
-    VkResult result = vkCreateInstance(&instance_info, NULL, &vulkan_instance);
+    VkResult result = vkCreateInstance(&instance_info, NULL, &graphics_vulkan_instance);
 
-    if(result != VK_SUCCESS || vulkan_instance == NULL) return GRAPHICS_VULKAN_INSTANCE_CREATE;
-
-    volkLoadInstanceOnly(vulkan_instance);
-
-#ifdef DEBUG
-    result = vkCreateDebugUtilsMessengerEXT(vulkan_instance, &debug_messenger_create_info, NULL, &debug_messenger);
-    assert(result == VK_SUCCESS);
-#endif
+    if(result != VK_SUCCESS || graphics_vulkan_instance == VK_NULL_HANDLE) return GRAPHICS_VULKAN_INSTANCE_CREATE;
 
     return GRAPHICS_OK;
 }
 
-void graphics_cleanup()
-{
-#ifdef DEBUG
-    vkDestroyDebugUtilsMessengerEXT(vulkan_instance, debug_messenger, NULL);
-#endif
 
-    vkDestroyInstance(vulkan_instance, NULL);
+static bool graphics_is_device_usable(
+                                        VkPhysicalDevice device,
+                                        const char* desired_extensions[],
+                                        uint32_t desired_extensions_count,
+                                        VkPhysicalDeviceFeatures* desired_features)
+{
+    uint32_t extension_count = 0;
+    vkEnumerateDeviceExtensionProperties(device, NULL, &extension_count, NULL);
+
+    VkExtensionProperties* extensions = (VkExtensionProperties*)malloc(extension_count * sizeof(VkExtensionProperties));
+    vkEnumerateDeviceExtensionProperties(device, NULL, &extension_count, extensions);
+
+    // TODO: LOW_PRIO Improve this loop
+    uint32_t found_extension_count = 0;
+    for(int i = 0; i < desired_extensions_count; i++)
+    {
+        for(int j = 0; j < extension_count; j++)
+        {
+            if(strcmp(desired_extensions[i], extensions[j].extensionName) == 0)
+            {
+                ++found_extension_count;
+            }
+        }
+    }
+
+    free(extensions);
+
+    bool extensions_present = found_extension_count == desired_extensions_count;
+    bool features_present = true;
+
+    VkPhysicalDeviceFeatures features;
+    vkGetPhysicalDeviceFeatures(device, &features);
+
+    for(uint32_t i = 0; i < sizeof(VkPhysicalDeviceFeatures) / sizeof(VkBool32); i++)
+    {
+        VkBool32* feature_field = ((VkBool32*)&features) + i;
+        VkBool32* desired_feature_field = ((VkBool32*)desired_features) + i;
+
+        if((*desired_feature_field) == VK_FALSE) continue;
+        if((*feature_field) != VK_TRUE) 
+        {
+            features_present = false;
+            break;
+        }
+    }
+
+    return extensions_present && features_present;
+}
+
+static VkPhysicalDevice graphics_pick_device(
+                                        const char* desired_extensions[],
+                                        uint32_t desired_extensions_count,
+                                        VkPhysicalDeviceFeatures* desired_features)
+{
+    VkPhysicalDevice picked_device = VK_NULL_HANDLE;
+    
+    uint32_t device_count = 0;
+    vkEnumeratePhysicalDevices(graphics_vulkan_instance, &device_count, NULL);
+
+    if(device_count == 0) return VK_NULL_HANDLE;
+
+    VkPhysicalDevice* devices = (VkPhysicalDevice*)malloc(device_count * sizeof(VkPhysicalDevice));
+    vkEnumeratePhysicalDevices(graphics_vulkan_instance, &device_count, devices);
+
+    uint16_t* device_usabilty = (uint16_t*)calloc(device_count, sizeof(uint16_t));
+    uint16_t highest_usability = 0;
+    uint32_t device_index_highest_usability = device_count;
+
+    for(uint32_t i = 0; i < device_count; i++)
+    {
+        if(!graphics_is_device_usable(devices[i], desired_extensions, desired_extensions_count, desired_features)) continue;
+
+        VkPhysicalDeviceProperties properties;
+        vkGetPhysicalDeviceProperties(devices[i], &properties);
+
+        VkPhysicalDeviceFeatures features;
+        vkGetPhysicalDeviceFeatures(devices[i], &features);
+
+        switch(properties.deviceType)
+        {
+            case VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU: device_usabilty[i] += 100; break;
+            case VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU:   device_usabilty[i] += 200; break;
+        }
+
+        for(uint32_t j = 0; j < sizeof(VkPhysicalDeviceFeatures) / sizeof(VkBool32); j++)
+        {
+            VkBool32* field = ((VkBool32*)&features) + j;
+            if(*field == VK_TRUE) device_usabilty[i]++;
+        }
+
+        if(highest_usability < device_usabilty[i]) 
+        {
+            highest_usability = device_usabilty[i];
+            device_index_highest_usability = i;
+        }
+    }
+
+    if(device_index_highest_usability != device_count) picked_device = devices[device_index_highest_usability];
+    
+    free(devices);
+    free(device_usabilty);
+
+    return picked_device;
+}
+
+static Graphics_queue_properties graphics_pick_queue_family(VkPhysicalDevice device)
+{
+    uint32_t queue_families_count = 0;
+    VkQueueFlags desired_properties = VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT;
+    Graphics_queue_properties queue_family_properties = {.index = UINT32_MAX, .properties = {}};
+
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_families_count, VK_NULL_HANDLE);
+
+    VkQueueFamilyProperties* queue_families = (VkQueueFamilyProperties*)malloc(queue_families_count * sizeof(VkQueueFamilyProperties));
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_families_count, queue_families);
+
+    for(uint32_t i = 0; i < queue_families_count; i++)
+    {
+        if((queue_families[i].queueCount > 0) && (queue_families[i].queueFlags & desired_properties == desired_properties))
+        {
+            queue_family_properties.index = i;
+            queue_family_properties.properties = queue_families[i];
+            break;
+        }
+    }
+
+    free(queue_families);
+
+    return queue_family_properties;
 }
